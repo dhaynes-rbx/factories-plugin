e1ec926109be85373bc2ee1cea8967f9e4144154
Dustin Haynes <dhaynes@roblox.com>
2023-01-06 12:31:35 -0800

    New constants module

---
+++ src/Constants.lua
@@ -0,0 +1,12 @@
+return {
+    Panels = {
+        InitializeFactoryUI = "InitializeFactoryUI",
+        EditFactoryUI = "EditFactoryUI",
+        EditMachinesListUI = "EditMachinesListUI",
+        EditMachineUI = "EditMachineUI",
+        EditItemsListUI = "EditItemsListUI",
+        EditItemUI = "EditItemUI",
+        EditPowerupsListUI = "EditPowerupsListUI",
+        EditPowerupUI = "EditPowerupUI",
+    }
+}
